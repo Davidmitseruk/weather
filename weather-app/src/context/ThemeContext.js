d21d6afabf8cfbe869c08a7a3a3c68597d2eb511
Davidmitseruk <davidmitseruk@gmail.com>
2025-11-17 16:53:55 +0100

    ok

---
+++ weather-app/src/context/ThemeContext.js
@@ -0,0 +1,11 @@
+import React, {createContext} from "react";
+export const ThemeContext = createContext();
+export function ThemeProvider({children}){
+
+
+    return(
+        <ThemeContext.Provider>{children}</ThemeContext.Provider>
+
+    )
+}
+
